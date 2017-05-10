7f2f1035ec7eab7fe6a21df1fa14cf02abea0770
mikeybkats <michaelbkats@gmail.com>
2017-05-09 22:18:33 -0700

    version which has launched.

--- source/javascripts/projects.js
+++ source/javascripts/projects.js
@@ -44,7 +44,7 @@ function isElementInViewport (el) {
   return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight ) && /*or $(window).height() */
       rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
   );
 }
